f826793bca4d4cc826d4f7d97fcc8db2223dab34
Genrey <genreycristobal99@gmail.com>
2023-07-07 04:50:46 +0800

    Server side

--- pangmasa/server/app.py
+++ pangmasa/server/app.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+import sqlite3
 
 app = Flask(__name__, template_folder='templates')
-app.run(debug=True)
 
-
-
-
+db = sqlite3.connect("store.db")
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/index")
+def home():
+    return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        request.form.get()
+
+    else:
+        return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        print("test")
+
+    else:
+        return render_template("login.html")
 
 
