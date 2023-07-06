c4e31d512052d6fdd3c919e33bec471e92ea3573
Genrey <genreycristobal99@gmail.com>
2023-07-06 09:35:53 +0800

    Server side settings

--- pangmasa/php/errors.php
+++
@@ -1,7 +0,0 @@
-<?php  if (count($errors) > 0) : ?>
-	<div class="error">
-		<?php foreach ($errors as $error) : ?>
-			<p class = "error" style = "color: red"><?php echo $error ?></p>
-		<?php endforeach ?>
-	</div>
-<?php  endif ?>
